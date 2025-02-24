5f9bf5d87a408bf4900d2f815375f008550c0e08
tharindutsb <146736670+tharindutsb@users.noreply.github.com>
2025-02-24 21:53:39 +0530

    update

--- utils/DB_Config.py
+++ utils/DB_Config.py
@@ -2,5 +2,13 @@
 CASE_COLLECTION = "case_distribution_drc"
 
 # Collection for transaction details
-TRANSACTION_COLLECTION = "case_distribution_drc_transactions"
+TRANSACTION_COLLECTION = "Case_distribution_drc_transactions"
 
+# Collection for drc summery
+CASE_DRC_SUMMERY="DRS_Database.Case_Distribution_DRC_Summary"
+
+# Collection for system task appending
+SYSTEM_TASKS="System_tasks"
+
+# collection for template task [config]
+TEMPLATE_TASK="Template_task"
--- utils/getCollection.py
+++ utils/getCollection.py
@@ -11,3 +11,6 @@ def get_collection_name(collection_key):
 # Test fetching the collection names
 print("Case Collection:", get_collection_name("CASE_COLLECTION"))
 print("Transaction Collection:", get_collection_name("TRANSACTION_COLLECTION"))
+print("Case DRC Summery",get_collection_name("CASE_DRC_SUMMERY"))
+print("System Tasks",get_collection_name("SYSTEM_TASKS"))
+print("Template Task",get_collection_name("TEMPLATE_TASK"))
