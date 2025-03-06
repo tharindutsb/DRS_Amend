12cde25d38e7dbaf29a3edb88bb1a18f05000ca6
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-03-06 11:04:31 +0530

    25_04_06_3

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# from actionManipulationCopy.task_processor import process_tasks 
-# from logger.loggers import get_logger
-
-# # Initialize logger
-# logger = get_logger("task_status_logger")
-                    
-# if __name__ == "__main__":
-#     logger.info("Starting task processing...")
-#     process_tasks()
-#     logger.info("Task processing completed.")
-
-
 from actionManipulationCopy.task_processor import process_tasks
 from utils.connectDB import get_db_connection, initialize_collections
 from logger.loggers import get_logger
