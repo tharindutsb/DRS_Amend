fde4fd04beb386f8173961ce63c9a9e404f1b5b9
tharindutsb <146736670+tharindutsb@users.noreply.github.com>
2025-03-24 15:22:05 +0530

    update

--- utils/loggers.py
+++ utils/loggers.py
@@ -2,10 +2,6 @@ import logging
 import logging.config
 import os
 
-# Ensure the logs directory exists
-logs_dir = os.path.join(os.path.dirname(__file__), "logs")
-os.makedirs(logs_dir, exist_ok=True)
-
 # Load logger configuration from loggers.ini
 config_file = os.path.join(os.path.dirname(__file__), "../config/loggers.ini")
 logging.config.fileConfig(config_file)
